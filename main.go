ea3ff872f2f82a859b01eb726b1f18292ce90bb8
Osfaldino <salhfeplar@gmail.com>
2026-09-21 18:39:00 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 )
 
+// struct for json data
 type Config struct {
 	AppName string   `json:"appName"`
 	Version string   `json:"version"`
@@ -13,16 +14,17 @@ type Config struct {
 	Users   []string `json:"users"`
 }
 
+// func to read file
 func (cfg *Config) ParseConfig(path string) error {
 	file, err := os.Open(path)
 
 	if err != nil {
 		return err
 	}
-	defer file.Close()
+	defer file.Close() // close file at the end
 
 	decoder := json.NewDecoder(file)
-	err = decoder.Decode(cfg)
+	err = decoder.Decode(cfg) // parse json into struct
 	if err != nil {
 		return err
 	}
@@ -31,12 +33,13 @@ func (cfg *Config) ParseConfig(path string) error {
 
 func main() {
 	var cfg Config
-	err := cfg.ParseConfig("cfg.json")
+	err := cfg.ParseConfig("cfg.json") // read config
 	if err != nil {
-		fmt.Printf("Ошибка: %s", err)
+		fmt.Printf("Error: %s", err)
 		os.Exit(1)
 	}
 
+	// print result
 	fmt.Printf("AppName: %s\nVersion: %s\n", cfg.AppName, cfg.Version)
 
 	for _, user := range cfg.Users {
